3c406970f9b1d427846ea370a76b9683ac5bfc98
Sophie Ferenczy <sophie.ferenczy@web.de>
2022-11-13 14:36:55 +0100

    Init Remix indie-stack.

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+/** @type {import('@types/eslint').Linter.BaseConfig} */
+module.exports = {
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "@remix-run/eslint-config/jest-testing-library",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  env: {
+    "cypress/globals": true,
+  },
+  plugins: ["cypress", "simple-import-sort"],
+  // we're using vitest which has a very similar API to jest
+  // (so the linting plugins work nicely), but it means we have to explicitly
+  // set the jest version.
+  settings: {
+    jest: {
+      version: 28,
+    },
+  },
+  rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
+  },
+};
